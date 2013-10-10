a6e960b58c4f0ed1e4134156e73546c0db8856d5
Ryan Page <rpage326@gmail.com>
2013-10-10 19:08:29 -0400

    Generated Migration File

--- main.rb
+++ main.rb
@@ -1,10 +1,13 @@
 require 'pry'
 require 'pg'
 require 'sinatra'
-require 'sinatra-reloader' if development?
-require 'sinatra-activerecord'
+require 'sinatra/reloader' if development?
+require 'sinatra/activerecord'
 require 'rake'
 
 set :database, {adapter:'postgresql',
   				database: 'ga-class-posts',
   				host: 'localhost'}
+
+class Post < ActiveRecord::Base
+end  
