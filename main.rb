2115a7094ec1c0f818c412076bc35558047ae439
Ben Winter <ben.g.winter@gmail.com>
2013-10-10 18:18:42 -0400

    working product, moving on to extra credit

--- main.rb
+++ main.rb
@@ -7,3 +7,40 @@ require 'sinatra/activerecord'
 set :database, {adapter: 'postgresql',
                 database: 'ga-hacker-news',
                 host: 'localhost'}
+
+class Post < ActiveRecord::Base
+end
+
+get '/' do 
+  @posts = Post.all
+  erb :post_index
+end
+
+get '/posts/:id/edit' do
+  @post = Post.find(params[:id])
+  erb :post_edit
+end
+
+post '/posts/:id/delete' do
+  Post.find(params[:id]).destroy
+  redirect '/'
+end
+
+get '/posts/new' do
+  erb :post_new
+end
+
+post '/posts/create' do
+  Post.create(title: params[:title], link: params[:link], body: params[:body], up_votes: 0, down_votes: 0)
+  redirect '/'
+end
+
+get '/posts/:id' do
+  @post = Post.find(params[:id])
+  erb :post_show
+end
+
+post '/posts/:id/update' do
+  Post.update(params[:id], :title => params[:title], :body => params[:body], :link => params[:link])
+  redirect '/'
+end
