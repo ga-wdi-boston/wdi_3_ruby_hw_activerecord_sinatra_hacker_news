fe1f30469112c7435b04bc86ffb8974495c17998
Ryan Page <rpage326@gmail.com>
2013-10-10 19:01:28 -0400

    Setup Gemfile

---
+++ main.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require 'pg'
+require 'sinatra'
+require 'sinatra-reloader' if development?
+require 'sinatra-activerecord'
+require 'rake'
+
+set :database, {adapter:'postgresql',
+  				database: 'ga-class-posts',
+  				host: 'localhost'}
