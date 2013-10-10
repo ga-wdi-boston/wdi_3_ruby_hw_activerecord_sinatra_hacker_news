000f72c9db5c4627f5bb8b58fe9994acd300931b
Ben Winter <ben.g.winter@gmail.com>
2013-10-10 17:26:22 -0400

    set up database, files and table

---
+++ main.rb
@@ -0,0 +1,9 @@
+require 'pg'
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'sinatra/activerecord'
+
+set :database, {adapter: 'postgresql',
+                database: 'ga-hacker-news',
+                host: 'localhost'}
