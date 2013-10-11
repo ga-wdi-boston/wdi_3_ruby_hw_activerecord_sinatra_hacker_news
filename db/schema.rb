6a8a7c2eaccdee39729b2a7eb2dd9cc5fff6ddda
Cori Allen <coriallen@gmail.com>
2013-10-11 10:16:32 -0400

    Adds HN favicon and nav bar styling.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131011112059) do
+ActiveRecord::Schema.define(version: 20131011135335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.string   "author"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
