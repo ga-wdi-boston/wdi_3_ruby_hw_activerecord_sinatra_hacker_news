682992ab13bbc78cbccc8e22fc23816ba87ab681
Matt Clement <darkmoves@gmail.com>
2013-10-10 23:43:07 -0400

    All functionality minus update.

--- main.rb
+++ main.rb
@@ -6,15 +6,6 @@ require 'sinatra/activerecord'
 
 set :database, {adapter: 'postgresql', database: 'hacknews', host: 'localhost'}
 
-   #  create_table :posts do |t|
-   #  	t.text :title
-   #  	t.text :link
-   #  	t.text :body
-   #  	t.integer :up_votes
-   #  	t.integer :down_votes
-   #  	t.timestamp :timestamp
-   # end
-
 class Post < ActiveRecord::Base
 end
 
@@ -27,20 +18,32 @@ get '/hackernews/new' do
 	erb :post_new
 end
 
+get '/hackernews/:id' do
+	@post = Post.find(params[:id])
+	erb :post_show
+end
+
 # This should be run by hitting the submit button on the /new page. <form action='hackernews/create' method='post'>COLLECT CONTENT</form>
 post '/hackernews/create' do
 	post = Post.create(title: params[:title], link: params[:link], body: params[:body], up_votes: 0, down_votes: 0)
 	redirect '/hackernews'
 end
 
-get '/hackernews/:id' do
-	@post = Post.find(params[:id])
-	erb :post_show
-end
-
-post '/hackernews/destroy' do
-
+post '/hackernews/:id/destroy' do
+	Post.find(params[:id]).delete
 	redirect '/hackernews'
 end
 
+post '/hackernews/:id/update' do
+	title = params[:title]
+	link = params[:link]
+	body = params[:body]
+	Post.find(params[:id]).update(title: title, link: link, body: body)
+	redirect '/hackernews/:id'
+end
+
+get '/hackernews/:id/edit' do
+	@post = Post.find(params[:id])
+	erb :post_edit
+end
 
