2370f2a0128995620d9e362956529509b7501822
Dan Crews <dancrews@me.com>
2013-10-13 16:03:29 -0400

    Added author fields and link to edit view.

--- main.rb
+++ main.rb
@@ -1,16 +1,17 @@
+# Gems required
 require 'pry'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'pg'
 require 'sinatra/activerecord'
 
-# Define the connection to the database
+# Define the connection to the database via Activerecord
 set :database, {adapter: 'postgresql',
 								database: 'danhacker',
 								host: 'localhost'}
 
 
-# Define the object classes for the datbases
+# Define the object classes for the databases
 class Story < ActiveRecord::Base
   has_many :comments
 end 
@@ -31,10 +32,10 @@ get '/new' do
 	erb :story_new
 end
 
-# This is for showing a  indivudual story and related comments
+# This is for showing an indivudual story and related comments
 get '/:id' do 
   @story = Story.find(params[:id])
-  erb :show_story
+  erb :story_show
 end
 
 # This is for editing a story
@@ -46,7 +47,8 @@ end
 
 # This is for posting a new story to the database
 post '/create' do 
-  Story.create(title: params[:title], 
+  Story.create(title: params[:title],
+                      author: params[:author], 
                        link: params[:link], 
                        body: params[:body])
   redirect '/'
@@ -55,6 +57,7 @@ end
 # This is for updating a story in the databae
 post '/:id/update' do 
   @story = Story.update(params[:id],
+                        author: params[:author],
                         title: params[:title],
                         link: params[:link],
                         body: params[:body])
