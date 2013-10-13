739336559a20408b54b6a88d7326a5edf93822b6
Dan Crews <dancrews@me.com>
2013-10-13 16:16:05 -0400

    Added default = 'anonymous' to story author column.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131012163232) do
+ActiveRecord::Schema.define(version: 20131013201112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20131012163232) do
     t.integer  "down_votes", default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "author"
+    t.string   "author",     default: "anonymous"
   end
 
 end
